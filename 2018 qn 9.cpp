92a1e89c0a6f8429d385d4fd7952536fb777817d
tamanghawana0107 <hawanatamang@gmail.com>
2022-05-11 00:30:54 +0545

    Added

--- 2018 qn 9.cpp
+++ 2018 qn 9.cpp
@@ -3,7 +3,7 @@ type int and float*/
 #include<iostream>
 using namespace std;
 template<typename R>
-void sum(R a[],R y)
+void sum(R a[],int y)
 {
 
 	cout<<"\nThe sum of first and last element in the array is :"<<a[0]+a[y-1];
@@ -12,8 +12,8 @@ void sum(R a[],R y)
 }
 int main()
 {
-	int b=0,c[30],n;
-	float d[30],m;
+	int b=0,c[30],n,m;
+	float d[30];
 	cout<<"Enter the number of elements you want to enter in integer  :";
 	cin>>n;
 	cout<<"Enter the elements :"<<endl;
@@ -22,13 +22,13 @@ int main()
 		cin>>c[b];
 	}
 	sum<int>(c,n);
-	/*cout<<"\nEnter the number of elements you want to enter in float  :";
+	cout<<"\n\nEnter the number of elements you want to enter in float  :";
 	cin>>m;
-	cout<<"\nEnter the elements :";
+	cout<<"Enter the elements :"<<endl;
 	for(b=0;b<m;b++)
 	{
 		cin>>d[b];
 	}
-	sum<float>(d,m);*/
+	sum<float>(d,m);
 	return 0;
 }
